84e817841689b967426f809af942322db840ed62
Ivan-Prystay <prystay.ivan@gmail.com>
2022-12-28 12:30:09 +0200

    fin, fin

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -34,10 +34,7 @@ export function ContactForm(props) {
         pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
         title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
         required
-        onChange={
-          //handleChange
-          e => setName(e.currentTarget.value)
-        }
+        onChange={e => setName(e.currentTarget.value)}
         value={name}
         id={NameInput.id}
       />
@@ -49,10 +46,7 @@ export function ContactForm(props) {
         pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
         title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
         required
-        onChange={
-          //handleChange
-          e => setNumber(e.currentTarget.value)
-        }
+        onChange={e => setNumber(e.currentTarget.value)}
         value={number}
         id={TelInput.id}
       />
